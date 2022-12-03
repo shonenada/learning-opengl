6088f78e7e491b8ae6b4edb583d9d47ef07cf04d
shonenada <shonenada@gmail.com>
2022-12-04 01:40:45 +0800

    feat: add common.h

--- src/light_casters_spotlight.cpp
+++ src/light_casters_spotlight.cpp
@@ -4,54 +4,11 @@
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include <iostream>
 
+#include "ofs/common.h"
 #include "ofs/shader.h"
 #include "ofs/camera.h"
 
-const int WIDTH = 1920;
-const int HEIGHT = 1080;
 const char* TITLE = "OpenGL - Lighting Map";
-
-void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
-void handleInput(GLFWwindow* window);
-void mouseCallback(GLFWwindow* window, double xPos, double yPos);
-void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
-void updateUniformColor(int shaderProgram);
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-float yaw = -90.0f;
-float pitch = 0.0f;
-Camera camera(cameraPos, cameraUp, yaw, pitch);
-
-glm::vec3 lightPos(1.2f, 0.5f, 2.0f);
-
-void errorCallback(int error, const char* description) {
-    std::cout << error << " " << description << std::endl;
-}
-
-unsigned int loadTexture(const char* path, GLenum format) {
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
-
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-
-    stbi_image_free(data);
-
-    return texture;
-}
 
 int main() {
     glfwSetErrorCallback(errorCallback);
@@ -254,66 +211,4 @@ int main() {
 
     glfwTerminate();
     return 0;
-}
-
-void frameBufferSizeCallback(GLFWwindow *window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
-
-void handleInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-    float currentFrame = glfwGetTime();
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-    float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-        camera.ProcessKeyboard(UP, deltaTime);
-    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-        camera.ProcessKeyboard(DOWN, deltaTime);
-
-    float LightMovementSpeed = 1;
-    float velocity = LightMovementSpeed * deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        lightPos.y += velocity;
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        lightPos.y -= velocity;
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-        lightPos.x -= velocity;
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-        lightPos.x += velocity;
-}
-
-float lastX = WIDTH / 2.0;
-float lastY = HEIGHT / 2.0;
-bool firstMouse = true;
-const float sensitivity = 0.1f;
-
-void mouseCallback(GLFWwindow* window, double xPos, double yPos) {
-    if (firstMouse) {
-        lastX = xPos;
-        lastY = yPos;
-        firstMouse = false;
-    }
-    float xOffset = xPos - lastX;
-    float yOffset = lastY - yPos;
-    lastX = xPos;
-    lastY = yPos;
-    camera.ProcessMouseMovement(xOffset, yOffset, true);
-}
-
-void scrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
-    camera.ProcessMouseScroll(yOffset);
 }
